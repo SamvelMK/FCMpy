cf1504f9b944738e428ef6903e5f66cdf594cf94
Samvel Mk <samuelmxitaryan@gmail.com>
2020-03-25 19:50:16 +0100

    removed the fcm.py file for now.

--- fcmbci/fcmbci/fcm.py
+++
@@ -1,9 +0,0 @@
-from data_processor.fcm_data import FcmDataProcessor
-
-class FcmBci(FcmDataProcessor):
-    def __init__(self):
-        
-        """ The FcmBci object initializes with a universe of discourse with a range [0,1].  """
-        
-        self.data = pd.DataFrame()
-        self.universe = np.arange(0, 1.01, 0.01)
