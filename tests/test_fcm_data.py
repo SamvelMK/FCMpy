586c923847ebe92edd87d78dfada39a6ba9dd46b
Samvel Mk <samuelmxitaryan@gmail.com>
2020-04-06 19:58:46 +0200

    added test_read_xlsx and test_consistency check.

--- tests/test_fcm_data.py
+++ tests/test_fcm_data.py
@@ -5,6 +5,7 @@ sys.path.insert(0, myPath + '/../')
 
 import unittest
 from fcmbci.data_processor.fcm_data import FcmDataProcessor
+from fcmbci.data_processor.process_functions import *
 import itertools
 import pandas as pd 
 
@@ -15,28 +16,43 @@ class TestDataProcessor(unittest.TestCase):
     def setUp(self):
         self.fcm = FcmDataProcessor()
 
+        self.fcm.read_xlsx('C:/PhD/FCM_Projects/FCM_Python/FCM_BCI/jupyter_prototype/sample_test.xlsx', 'Matrix')
+        self.data_mat = self.fcm.data
+        
+        self.fcm.read_xlsx('C:/PhD/FCM_Projects/FCM_Python/FCM_BCI/jupyter_prototype/list_format.xlsx', 'List')
+        self.data_lst = self.fcm.data
+
     def test_read_excel(self):
-        self.fcm.read_xlsx('C:/PhD/FCM_Projects/FCM_Python/FCM_BCI/jupyter_prototype/sample_test.xlsx')
-        data = self.fcm.data
-        
-        # Checks whether the data was successfully read or not. 
-        self.assertIsNotNone(data)
+        self.assertIsNotNone(self.data_mat)
+        self.assertIsNotNone(self.data_lst)
     
-    def test_atumf(self):
-        results = self.fcm.automf(self.fcm.universe, linguistic_terms=['-VH', '-H', '-M', '-L', '-VL', 'VL','L', 'M', 'H', 'VH'])
-
-        # Checks whether the returned object is of type dict.
-        self.assertEqual(type(results), dict)
-
-        # Checks whether the MFs are generated for all the linguistic terms.
-        self.assertEqual(len(results.keys()), 10)
-
-        # Check whether the values of the MF are within the fuzzy range 0,1.
-        res_max = max(list(itertools.chain(*[list(results[i]) for i in results])))
-        res_min = min(list(itertools.chain(*[list(results[i]) for i in results])))
+    def test_consistency_check(self):
+        dm = self.data_mat
+        dm['Expert_1'].loc['C1','C2'] = "-VH" # Adds inconsistency in the raitings.
+        with self.assertRaises(ValueError):
+            consistency_check(dm, dtype = 'Matrix')
         
-        self.assertLessEqual(res_max, 1)
-        self.assertGreaterEqual(res_min, 0)
+        dl = self.data_lst
+        dl['Expert_1'] = dl['Expert_1'].replace(-1, 1)
+        with self.assertRaises(ValueError):
+            consistency_check(dl, dtype = 'List')
+        
+
+    # def test_atumf(self):
+    #     results = self.fcm.automf(self.fcm.universe, linguistic_terms=['-VH', '-H', '-M', '-L', '-VL', 'VL','L', 'M', 'H', 'VH'])
+
+    #     # Checks whether the returned object is of type dict.
+    #     self.assertEqual(type(results), dict)
+
+    #     # Checks whether the MFs are generated for all the linguistic terms.
+    #     self.assertEqual(len(results.keys()), 10)
+
+    #     # Check whether the values of the MF are within the fuzzy range 0,1.
+    #     res_max = max(list(itertools.chain(*[list(results[i]) for i in results])))
+    #     res_min = min(list(itertools.chain(*[list(results[i]) for i in results])))
+        
+    #     self.assertLessEqual(res_max, 1)
+    #     self.assertGreaterEqual(res_min, 0)
     
     # def test_activate(self):
     #     mf = self.fcm.automf(linguistic_terms = ['VL', 'L'])
