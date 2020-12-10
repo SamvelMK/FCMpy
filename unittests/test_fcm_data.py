f4649004e666256c5ea61b3887cfaf6d4abcdf33
SamvelMK <mkhitarian.samvel@gmail.com>
2020-12-10 18:58:05 +0100

    some more unittests for the methods based on the changes in the class.

--- unittests/test_fcm_data.py
+++ unittests/test_fcm_data.py
@@ -22,75 +22,68 @@ class TestDataProcessor(unittest.TestCase):
         self.assertEqual(self.fcm.linguistic_terms, ['-VH', '-H', '-M', '-L', '-VL', 'VL','L', 'M', 'H', 'VH'], msg='Error in the creating an instance of the class!')
 
     def test_read_excel(self):    
-        self.fcm.read_xlsx(os.path.abspath('unittests/test_cases/data.xlsx'))
+        self.fcm.read_xlsx(os.path.abspath('unittests/test_cases/data_test.xlsx'))
         self.assertIsNotNone(self.fcm.data)
+        self.assertIsInstance(self.fcm.data, collections.OrderedDict)
+        self.assertEqual(len(self.fcm.data), 6)
+        self.assertEqual(len(self.fcm.data['Expert_1'].columns), 7)
+
     def test_read_json(self):
         self.fcm.read_json(os.path.abspath('unittests/test_cases/data_test.json'))
         self.assertIsNotNone(self.fcm.data)
+        self.assertIsInstance(self.fcm.data, collections.OrderedDict)
+        self.assertEqual(len(self.fcm.data), 2)
+        self.assertEqual(len(self.fcm.data['Expert_1'].columns), 8) # plus the NA column.
+
+    def test_atumf(self):
+        lt = ['-VH', '-H', '-M', '-L', '-VL', 'VL','L', 'M', 'H', 'VH']
+        results = self.fcm.automf(self.fcm.universe, 
+                                    linguistic_terms=lt)
+        # Checks whether the returned object is of type dict.
+        self.assertEqual(type(results), dict)
+
+        # Checks whether the MFs are generated for all the linguistic terms.
+        self.assertEqual(len(results.keys()), len(lt))
+
+        # Check whether the values of the MF are within the fuzzy range 0,1.
+        res_max = max(list(itertools.chain(*[list(results[i]) for i in results])))
+        res_min = min(list(itertools.chain(*[list(results[i]) for i in results])))
+        
+        self.assertLessEqual(res_max, 1)
+        self.assertGreaterEqual(res_min, 0)
     
-    # def test_consistency_check(self):
-
-    #     # For matrix-like format
-    #     dm = self.data_mat
-    #     dm['Expert_1'].loc['C1','C2'] = "-VH" # Adds inconsistency in the raitings.
-    #     with self.assertRaises(ValueError):
-    #         consistency_check(dm, dtype = 'Matrix')
-        
-    #     # For list_format
-    #     dl = self.data_lst
-    #     dl['Expert_1'] = dl['Expert_1'].replace(-1, 1) # Adds inconsistency in the raitings.
-    #     with self.assertRaises(ValueError):
-    #         consistency_check(dl, dtype = 'List')
-        
-
-    # def test_atumf(self):
-    #     results = self.fcm.automf(self.fcm.universe, 
-    #                                 linguistic_terms=['-VH', '-H', '-M', '-L', '-VL', 'VL','L', 'M', 'H', 'VH'])
-    #     # Checks whether the returned object is of type dict.
-    #     self.assertEqual(type(results), dict)
-
-    #     # Checks whether the MFs are generated for all the linguistic terms.
-    #     self.assertEqual(len(results.keys()), 10)
-
-    #     # Check whether the values of the MF are within the fuzzy range 0,1.
-    #     res_max = max(list(itertools.chain(*[list(results[i]) for i in results])))
-    #     res_min = min(list(itertools.chain(*[list(results[i]) for i in results])))
-        
-    #     self.assertLessEqual(res_max, 1)
-    #     self.assertGreaterEqual(res_min, 0)
+    def test_activate(self):
+        res = self.fcm.automf(self.fcm.universe, 
+                                linguistic_terms = ['-VH', '-H', '-M', '-L', '-VL', 'VL','L', 'M', 'H', 'VH'])
     
-    # def test_activate(self):
-    #     res = self.fcm.automf(self.fcm.universe, 
-    #                             linguistic_terms = ['-VH', '-H', '-M', '-L', '-VL', 'VL','L', 'M', 'H', 'VH'])
-    
-    #     act = self.fcm.activate({'M': 0.16, 'H': 0.5, 'VH': 0.33}, res)
-    #     # Checks whether the activated membership functions are within the fuzzy range 0,1.
-        
-    #     for i in act:
-    #         self.assertLessEqual(max(act[i]), 1)
-    #         self.assertGreaterEqual(min(act[i]), 0)
+        act = self.fcm.activate({'M': 0.16, 'H': 0.5, 'VH': 0.33}, res)
 
-    # def test_aggregate(self):
-    #     res = self.fcm.automf(self.fcm.universe,
-    #                             linguistic_terms = ['-VH', '-H', '-M', '-L', '-VL', 'VL','L', 'M', 'H', 'VH'])
-    #     act = self.fcm.activate({'M': 0.16, 'H': 0.5, 'VH': 0.33}, res)
-    #     aggr = self.fcm.aggregate(act)
-        
-    #     # the length of the aggregated function should be equal to that of the universe.
-    #     self.assertEqual(len(aggr), len(self.fcm.universe))
-        
-    #     # Checks whether the aggregated function is in the fuzzy range. 
-    #     self.assertLessEqual(max(aggr), 1)
-    #     self.assertGreaterEqual(min(aggr), 0)
+        # Checks whether the activated membership functions are within the fuzzy range 0,1.
+        for i in act:
+            self.assertLessEqual(max(act[i]), 1)
+            self.assertGreaterEqual(min(act[i]), 0)
 
-    # def test_defuzzify(self):
-    #     res = self.fcm.automf(self.fcm.universe,
-    #                             linguistic_terms = ['-VH', '-H', '-M', '-L', '-VL', 'VL','L', 'M', 'H', 'VH'])
-    #     act = self.fcm.activate({'M': 0.16, 'H': 0.5, 'VH': 0.33}, res)
-    #     aggr = self.fcm.aggregate(act)
+    def test_aggregate(self):
+        res = self.fcm.automf(self.fcm.universe,
+                                linguistic_terms = ['-VH', '-H', '-M', '-L', '-VL', 'VL','L', 'M', 'H', 'VH'])
+        act = self.fcm.activate({'M': 0.16, 'H': 0.5, 'VH': 0.33}, res)
+        aggr = self.fcm.aggregate(act)
         
-    #     defuz_res = self.fcm.defuzzify(self.fcm.universe, aggr, method = 'centroid')
-    #     self.assertAlmostEqual(defuz_res, 0.70, 2)
+        # the length of the aggregated function should be equal to that of the universe.
+        self.assertEqual(len(aggr), len(self.fcm.universe))
+        
+        # Checks whether the aggregated function is in the fuzzy range. 
+        self.assertLessEqual(max(aggr), 1)
+        self.assertGreaterEqual(min(aggr), 0)
+
+    def test_defuzzify(self):
+        res = self.fcm.automf(self.fcm.universe,
+                                linguistic_terms = ['-VH', '-H', '-M', '-L', '-VL', 'VL','L', 'M', 'H', 'VH'])
+        act = self.fcm.activate({'M': 0.16, 'H': 0.5, 'VH': 0.33}, res)
+        aggr = self.fcm.aggregate(act)
+        
+        defuz_res = self.fcm.defuzzify(self.fcm.universe, aggr, method = 'centroid')
+        self.assertAlmostEqual(defuz_res, 0.70, 2)
     
     # def test_gen_weights_mat(self):
     #     self.fcm.gen_weights_mat(self.data_mat)
