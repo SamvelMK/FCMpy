cb5f9e8a2f9f0f8f90631be02f540051877813e4
SamvelMK <mkhitarian.samvel@gmail.com>
2021-01-15 11:57:53 +0100

    added the intervention module to the name base

--- fcmbci/__init__.py
+++ fcmbci/__init__.py
@@ -1,5 +1,5 @@
 
-
 from .data_processor.data_processor import FcmDataProcessor
 from .visualization.fcm_view import FcmVisualize
 from .simulator.simulator import FcmSimulator
+from .intervention.intervention import Intervention
