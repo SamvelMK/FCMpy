a940260ae72dd7dea11e42789c4d2e0fba56351b
SamvelMK <mkhitarian.samvel@gmail.com>
2021-04-22 10:45:41 +0200

    implimentation of the Defuzzification interface

---
+++ fcmpy/expert_fcm/defuzz.py
@@ -0,0 +1,158 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
+import numpy as np
+from expert_fcm.input_validator import type_check
+from abc import ABC, abstractclassmethod 
+import skfuzzy as fuzz
+
+class Defuzzification(ABC):
+    
+    """
+    Defuzzification methods.
+    """
+
+    @abstractclassmethod
+    def defuzz():
+        raise NotImplementedError('defuzzification method is not defined!')
+
+class Centroid(Defuzzification):
+    
+    """
+    Centroid difuzzification method (i.e., center of gravity).
+    """
+
+    @staticmethod
+    @type_check
+    def defuzz(**kwargs) -> float:
+
+        """
+        Centroid difuzzification method (i.e., center of gravity).
+        
+        Other Parameters
+        ----------
+        **x: numpy.ndarray
+            universe of discourse 
+        
+        **mfx: numpy.ndarray,
+                    "aggregated" membership functions
+        """
+
+        method = kwargs['method']
+        x = kwargs['x']
+        mfx = kwargs['mfx']
+
+        return fuzz.defuzz(x, mfx, method)
+
+class Bisector(Defuzzification):
+
+    """
+    Bisector difuzzification method.
+    """
+
+    @staticmethod
+    @type_check
+    def defuzz(**kwargs) -> float:
+
+        """
+        Bisector difuzzification method.
+        
+        Other Parameters
+        ----------
+        **x: numpy.ndarray
+            universe of discourse 
+        
+        **mfx: numpy.ndarray,
+                    "aggregated" membership functions
+        """
+
+        method = kwargs['method']
+        x = kwargs['x']
+        mfx = kwargs['mfx']
+
+        return fuzz.defuzz(x, mfx, method)
+
+class MeanOfMax(Defuzzification):
+
+    """
+    MeanOfMax difuzzification method.
+    """
+
+    @staticmethod
+    @type_check
+    def defuzz(**kwargs) -> float:
+
+        """
+        MeanOfMax difuzzification method.
+        
+        Other Parameters
+        ----------
+        **x: numpy.ndarray
+            universe of discourse 
+        
+        **mfx: numpy.ndarray,
+                    "aggregated" membership functions
+        """
+
+        method = kwargs['method']
+        x = kwargs['x']
+        mfx = kwargs['mfx']
+
+        return fuzz.defuzz(x, mfx, method)
+
+class MinOfMax(Defuzzification):
+
+    """
+    MinOfMax difuzzification method.
+    """
+
+    @staticmethod
+    @type_check
+    def defuzz(**kwargs) -> float:
+
+        """
+        MinOfMax difuzzification method.
+        
+        Other Parameters
+        ----------
+        **x: numpy.ndarray
+            universe of discourse 
+        
+        **mfx: numpy.ndarray,
+                    "aggregated" membership functions
+        """
+
+        method = kwargs['method']
+        x = kwargs['x']
+        mfx = kwargs['mfx']
+
+        return fuzz.defuzz(x, mfx, method)
+
+class MaxOfMax(Defuzzification):
+
+    """
+    MaxOfMax difuzzification method.
+    """
+
+    @staticmethod
+    @type_check
+    def defuzz(**kwargs) -> float:
+
+        """
+        MaxOfMax difuzzification method.
+        
+        Other Parameters
+        ----------
+        **x: numpy.ndarray
+            universe of discourse 
+        
+        **mfx: numpy.ndarray,
+                    "aggregated" membership functions
+        """
+
+        method = kwargs['method']
+        x = kwargs['x']
+        mfx = kwargs['mfx']
+
+        return fuzz.defuzz(x, mfx, method)
