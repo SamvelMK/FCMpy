7379617c839edbaed4b1b69928c531354f62dc9f
SamvelMK <mkhitarian.samvel@gmail.com>
2021-04-22 10:50:24 +0200

    Class of methods for data transformations

---
+++ fcmpy/expert_fcm/transform.py
@@ -0,0 +1,66 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
+import pandas as pd
+from typing import Union
+from fcmpy.expert_fcm.input_validator import type_check
+import collections
+
+class Transform:
+    
+    """
+    Create a Flat data and Calculate the proportions of answers to each linguistic term.
+    """
+
+    @staticmethod
+    @type_check
+    def calculateProportions(data: pd.DataFrame, conceptPair: tuple, nExperts:int) -> dict:
+
+        """
+        Calculate the proportions of answers to each linguistic term.
+
+        Parameters
+        ----------
+        data: pandas.DataFrame
+                     Data frame that contains all the expert inputs (i.e., "flattened" OrderedDict)
+        
+        conceptPair: tuple,
+                        concept pair for which the activation parameter should be constructed
+
+        Return
+        ---------
+        y: dict,
+            keys ---> linguistic terms, values ---> proportion of expert raitings.
+        """
+
+        activation_parameter = {}
+        activation_parameter = (data.loc[conceptPair].sum()/nExperts).to_dict()
+
+        return activation_parameter
+
+    @staticmethod
+    @type_check
+    def flatData(data: Union[dict, collections.OrderedDict]) -> pd.DataFrame:
+
+        """
+        Create a flat data from an ordered dictionary.
+
+        Parameters
+        ----------
+        data: dict,
+                keys ---> expertId, values ---> pandas.DataFrame
+        
+        Return
+        ---------
+        y: pandas.DataFrame
+            data with all the expert inputs in one dataframe.
+        """
+
+        # Create a flat data with all of the experts' inputs.
+        flat_data = pd.concat([data[i] for i in data], sort = False)
+        flat_data.columns = [i.lower() for i in flat_data.columns]
+        flat_data = flat_data.set_index(['from', 'to'])
+        flat_data = flat_data.sort_index(level=['from','to'])
+
+        return flat_data
