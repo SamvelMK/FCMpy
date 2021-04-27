bf952c466667b84e16069a709e5da9efa3584604
SamvelMK <mkhitarian.samvel@gmail.com>
2021-04-27 21:50:59 +0200

    added interface GetMethod

--- fcmpy/expert_fcm/methodsStore.py
+++ fcmpy/expert_fcm/methodsStore.py
@@ -10,8 +10,21 @@ from fcmpy.expert_fcm.membership import TriangularMembership, GaussianMembership
 from fcmpy.expert_fcm.implication import Mamdani, Larsen
 from fcmpy.expert_fcm.aggregation import Fmax, AlgSum, EinsteinSum, HamacherSum
 from fcmpy.expert_fcm.defuzz import Centroid, Bisector, MeanOfMax, MinOfMax, MaxOfMax
+from fcmpy.expert_fcm.input_validator import type_check
 
-class ReaderStore:
+from abc import ABC, abstractmethod
+
+class GetMethod(ABC):
+
+    """
+    Get methods from a store.
+    """
+    
+    @abstractmethod
+    def get():
+        raise NotImplementedError('Get method is not defined!')
+
+class ReaderStore(GetMethod):
 
     """
     Methods of reading data files.
@@ -20,13 +33,14 @@ class ReaderStore:
     __methods = {'csv' : CSV, 'xlsx' : XLSX, 'json' : JSON}
 
     @staticmethod
-    def getReader(method):
+    @type_check
+    def get(method:str):
         if method in ReaderStore.__methods.keys():
             return ReaderStore.__methods[method]
         else:
             raise ValueError('The reader method is not defined.')
 
-class EntropyStore:
+class EntropyStore(GetMethod):
 
     """
     Methods of calculating entropy.
@@ -35,13 +49,14 @@ class EntropyStore:
     __methods = {'entropy' : InformationEntropy}
 
     @staticmethod
-    def getEnrtopy(method):
+    @type_check
+    def get(method:str):
         if method in EntropyStore.__methods.keys():
             return EntropyStore.__methods[method]
         else:
             raise ValueError('The enrtopy method is not defined.')
 
-class MembershipStore:
+class MembershipStore(GetMethod):
 
     """
     Methods of generating membership functions.
@@ -50,13 +65,14 @@ class MembershipStore:
     __methods = {'trimf' : TriangularMembership, 'gaussmf': GaussianMembership, 'trapmf' : TrapezoidalMembership}
 
     @staticmethod
-    def getMembership(method):
+    @type_check
+    def get(method:str):
         if method in MembershipStore.__methods.keys():
             return MembershipStore.__methods[method]
         else:
             raise ValueError('The membership method is not defined.')
 
-class ImplicationStore:
+class ImplicationStore(GetMethod):
 
     """
     Fuzzy implication rules.
@@ -65,13 +81,14 @@ class ImplicationStore:
     __methods = {'Mamdani' : Mamdani, 'Larsen' : Larsen}
 
     @staticmethod
-    def getImplication(method):
+    @type_check
+    def get(method:str):
         if method in ImplicationStore.__methods.keys():
             return ImplicationStore.__methods[method]
         else:
             raise ValueError('The implication method is not defined.')
 
-class AggregationStore:
+class AggregationStore(GetMethod):
 
     """
     Fuzzy aggregation rules.
@@ -80,13 +97,14 @@ class AggregationStore:
     __methods = {'fMax' : Fmax, 'algSum' : AlgSum, 'eSum' : EinsteinSum, 'hSum' : HamacherSum}
 
     @staticmethod
-    def getAggregation(method):
+    @type_check
+    def get(method:str):
         if method in AggregationStore.__methods.keys():
             return AggregationStore.__methods[method]
         else:
             raise ValueError('The aggregation method is not defined.')
 
-class DefuzzStore:
+class DefuzzStore(GetMethod):
 
     """
     Defuzzification methods.
@@ -95,7 +113,8 @@ class DefuzzStore:
     __methods = {'centroid' : Centroid, 'bisector' : Bisector, 'mom' : MeanOfMax, 'som' : MinOfMax, 'lom' : MaxOfMax}
 
     @staticmethod
-    def getDefuzz(method):
+    @type_check
+    def get(method:str):
         if method in DefuzzStore.__methods.keys():
             return DefuzzStore.__methods[method]
         else:
