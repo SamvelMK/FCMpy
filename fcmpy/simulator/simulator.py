b67f9ddbb7dc8a90a34c13ae852c2a85157dbac6
SamvelMK <mkhitarian.samvel@gmail.com>
2021-07-04 16:20:17 +0200

    added the implementation of convergence in the simulator.

--- fcmpy/simulator/simulator.py
+++ fcmpy/simulator/simulator.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from fcmpy.simulator.methodStore import InferenceStore, TransferStore
+from fcmpy.simulator.methodStore import InferenceStore, TransferStore, ConvergenceStore
 import warnings
 from fcmpy.expert_fcm.input_validator import type_check
 from typing import Union
@@ -19,7 +19,7 @@ class Simulator(ABC):
 class FcmSimulator(Simulator):
 
     """
-    The class includes methods for runing sumulations on top of a defined FCM.
+    The class includes methods for runing simulations on top of a defined FCM.
 
     Methods:
         simulate(initial_state: dict, weight_matrix: Union[pd.DataFrame, np.ndarray], 
@@ -55,7 +55,8 @@ class FcmSimulator(Simulator):
     @staticmethod
     @type_check
     def simulate(initial_state: dict, weight_matrix: Union[pd.DataFrame, np.ndarray], 
-                        transfer: str, inference: str, thresh:float=0.001, iterations:int=50, **kwargs) -> pd.DataFrame:
+                        transfer: str, inference: str, thresh:float=0.001, iterations:int=50, 
+                        output_concepts = None, convergence = 'absDiff', **kwargs) -> pd.DataFrame:
         
         """
         Runs simulations over the passed FCM.
@@ -80,7 +81,15 @@ class FcmSimulator(Simulator):
 
         iterations: int
                         number of iterations
-                        
+
+        output_concepts: bool, list
+                            the output concepts for the convergence check
+                            default --> None
+
+        convergence: str,
+                        convergence method
+                        default --> 'absDiff': absolute difference between the simulation steps
+
         params: additional parameters for the methods
 
         Return
@@ -96,6 +105,12 @@ class FcmSimulator(Simulator):
         else:
             warnings.warn("When passing an initial state with a weight matrix type numpy.ndarray make sure that the order of the keys in the dictionary with the initial states matches the order of the column of the numpy.ndarray!")
 
+        # Check if output concepts are in the initial_state.keys()
+        if output_concepts:
+            for i in output_concepts:
+                if i not in initial_state.keys():
+                    raise ValueError(f'The specified output concept {i} is not in the list.')
+
         # create the empty dataframe for the results
         results = pd.DataFrame(initial_state, index=[0])
         state_vector = np.array(list(initial_state.values()))
@@ -104,15 +119,17 @@ class FcmSimulator(Simulator):
         stableConceptPos = FcmSimulator.__getStableConcepts(weight_matrix=weight_matrix.T)
         satble_values = state_vector[stableConceptPos]
 
-        step_count = 0
-        residual = thresh
-        
+        # get the methods for the simulation.
         transfer = TransferStore.get(transfer)()
         inference = InferenceStore.get(inference)()
+        conv = ConvergenceStore.get(convergence)()
         
-        while step_count <= iterations:
-            if (residual >= thresh):
-                
+        # initialize params
+        convergenceStatus = False
+        step_count = 0
+
+        for i in range(iterations):
+            if not convergenceStatus:    
                 infered = inference.infer(initial_state=state_vector, weight_matrix=weight_matrix, params=kwargs)
                 state_vector = transfer.transfer(x=infered, params=kwargs)
                 
@@ -126,15 +143,13 @@ class FcmSimulator(Simulator):
                 step_count +=1
                 
                 # compute the residuals between the steps.
-                residual = max(abs(results.loc[len(results)-1] - results.loc[len(results) - 2]))
-                
-                if step_count >= iterations:
-                    warnings.warn("The values didn't converged. More iterations are required!")
-                else:
-                    pass
-                
-            else: # if the residual < threshold print the step and exit the loop.
+                convergenceStatus = conv.check_convergence(output_concepts=output_concepts, results=results, threshold = thresh, params=kwargs) 
+
+            else:    
                 print(f'The values converged in the {step_count+1} state (e <= {thresh})')
                 break
-        
+
+            if step_count >= iterations:
+                warnings.warn("The values didn't converge. More iterations are required!")
+
         return results
