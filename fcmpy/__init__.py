3ce4ffc522b62af8743ca165df23291feef2a04e
SamvelMK <mkhitarian.samvel@gmail.com>
2021-09-25 20:12:22 +0200

    modified the namespace

--- fcmpy/__init__.py
+++ fcmpy/__init__.py
@@ -1,4 +1,3 @@
 from .expert_fcm.expert_based_fcm import ExpertFcm
 from .simulator.simulator import FcmSimulator
 from .intervention.intervention import FcmIntervention
-from .ML.hebbian import NHL
