f8e824b7d32b485333a25bf3eae5dde0f1e30292
SamvelMK <mkhitarian.samvel@gmail.com>
2021-10-24 23:53:18 +0200

    added validation import

--- fcmpy/__init__.py
+++ fcmpy/__init__.py
@@ -1,3 +1,10 @@
 from .expert_fcm.expert_based_fcm import ExpertFcm
 from .simulator.simulator import FcmSimulator
 from .intervention.intervention import FcmIntervention
+from .ml.hebbian.nhl import NHL
+from .ml.hebbian.ahl import AHL
+from .ml.genetic.rcga import RCGA
+from .ml.validation import HebbianValidate
+from .ml.validation import ISE
+from .ml.validation import OSE
+
