94b0a7583cf89f044a879f8c05fc7544830481f2
SamvelMK <mkhitarian.samvel@gmail.com>
2022-04-27 22:00:26 +0200

    added mt, vt = 0 initializations

--- fcmpy/ml/gradient_descent/weight_update.py
+++ fcmpy/ml/gradient_descent/weight_update.py
@@ -1,11 +1,39 @@
 from abc import ABC, abstractmethod
-
+import numpy as np
 
 class WeightUpdate(ABC):
     """
-        Interface for updating the parameter matrix W.
+        Interface for computing the change in the parameter matrix W.
     """
     @abstractmethod
     def update(**kwargs):
         raise NotImplementedError('update method is not defined.')
 
+
+class VanillaGd(WeightUpdate):
+    @staticmethod
+    def update(**kwargs):
+        delta_w = kwargs['delta_w']
+        learning_rate = kwargs['learning_rate']
+        return -learning_rate*delta_w
+
+
+class Adam(WeightUpdate):
+    @staticmethod
+    def update(**kwargs):
+        b1 = kwargs['b1']
+        b2 = kwargs['b2']
+        mt = 0
+        vt=0
+        delta_w = kwargs['delta_w']
+        epoch = kwargs['epoch']
+        learning_rate = kwargs['learning_rate']
+        e = kwargs['e']
+        
+        mt = b1*mt+(1-b1)*delta_w
+        vt = b2*vt+(1-b2)*(delta_w)**2
+        mthat = mt/(1-b1**(epoch+1))
+        vthat = vt/(1-b2**(epoch+1))
+        change = -(learning_rate/(np.sqrt(vthat)+e))*mthat
+
+        return change
