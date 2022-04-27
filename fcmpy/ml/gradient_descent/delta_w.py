518a77b4b3062692faa59e1350284fd1943c02e2
SamvelMK <mkhitarian.samvel@gmail.com>
2022-04-27 21:58:52 +0200

    debugged the partial derivatives

--- fcmpy/ml/gradient_descent/delta_w.py
+++ fcmpy/ml/gradient_descent/delta_w.py
@@ -1,8 +1,5 @@
-from matplotlib.font_manager import _Weight
 import numpy as np
 from abc import ABC, abstractmethod
-from fcmpy.store.methodsStore import InferenceStore
-from fcmpy.store.methodsStore import TransferStore
 from fcmpy.store.methodsStore import GradientStore
 
 class Update(ABC):
@@ -19,21 +16,16 @@ class DeltaW(Update):
         Calculate the change of the parameter matrix W.
     """
     @staticmethod
-    def calculate(inference:str, transfer:str, data:np.array, state_vector, weight_matrix, **kwargs):
+    def calculate(data:np.array, simulated, state_vector, weight_matrix, transfer:str, inference:str, **kwargs):
         """
             Calculate the change of the parameter matrix W.
         """
-        infer = InferenceStore.get(method=inference).infer
-        trans = TransferStore.get(method=transfer).transfer
-        dx_squared = GradientStore.get(method='dx_squared').compute
+        dx_squared = GradientStore.get(method='dxSquared').compute
         dx_inference = GradientStore.get(method=inference).compute
         dx_transfer = GradientStore.get(method=transfer).compute
 
-        infered  = infer(initial_state = state_vector, weight_matrix=weight_matrix)
-        simulated = trans(x=infered, params=kwargs)
-
         dx_error = dx_squared(observed=data, simulated=simulated)
         dx_infer = dx_inference(state_vector=state_vector)
-        dx_trans = dx_transfer(x=infered, params=kwargs)
-
+        dx_trans = dx_transfer(x=state_vector, weight_matrix = weight_matrix, inference=inference, params=kwargs)
+    
         return (dx_error * dx_trans * dx_infer)*np.abs(np.sign(weight_matrix))
