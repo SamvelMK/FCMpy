50c0554c1cab4956f6b8db17bb41336abd2a5754
SamvelMK <mkhitarian.samvel@gmail.com>
2022-04-28 14:57:58 +0200

    added docstring to deltaW

--- fcmpy/ml/gradient_descent/delta_w.py
+++ fcmpy/ml/gradient_descent/delta_w.py
@@ -13,18 +13,38 @@ class Update(ABC):
 
 class DeltaW(Update):
     """
-        Calculate the change of the parameter matrix W.
+        Calculate the change of the parameter matrix W for MSE loss function.
     """
     @staticmethod
-    def calculate(data:np.array, simulated, state_vector, weight_matrix, transfer:str, inference:str, **kwargs):
+    def calculate(data:np.array, predicted:np.array, state_vector:np.array, weight_matrix:np.array, transfer:str, inference:str, **kwargs):
         """
             Calculate the change of the parameter matrix W.
+
+            Parameters
+            ----------
+            data: np.array
+                    historical data
+
+            predicted: np.array
+                        predicted data
+            
+            state_vector: np.array
+                            the concept values at time point t
+            
+            weight_matrix: np.array
+                            the FCM connection matrix at time point t
+            
+            transfer: str
+                        type of transfer method
+            
+            inference: str
+                        type of inference method
         """
         dx_squared = GradientStore.get(method='dxSquared').compute
         dx_inference = GradientStore.get(method=inference).compute
         dx_transfer = GradientStore.get(method=transfer).compute
 
-        dx_error = dx_squared(observed=data, simulated=simulated)
+        dx_error = dx_squared(observed=data, simulated=predicted)
         dx_infer = dx_inference(state_vector=state_vector)
         dx_trans = dx_transfer(x=state_vector, weight_matrix = weight_matrix, inference=inference, params=kwargs)
     
